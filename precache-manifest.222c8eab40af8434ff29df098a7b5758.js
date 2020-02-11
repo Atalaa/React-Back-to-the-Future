9abf4eb1fcf193d9751a207ce4fca54f87da0945
Atala <atala.rchidi@gmail.com>
2020-02-10 19:43:29 -0500

    Updates

---
+++ precache-manifest.222c8eab40af8434ff29df098a7b5758.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26a469ea6b1dd5652f3bfdf8fbb121d2",
+    "url": "/Back-to-the-Future/index.html"
+  },
+  {
+    "revision": "ba508082542846c51dd1",
+    "url": "/Back-to-the-Future/static/js/2.9775b4e6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Back-to-the-Future/static/js/2.9775b4e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc0154553c8491b676e9",
+    "url": "/Back-to-the-Future/static/js/main.5e854a8b.chunk.js"
+  },
+  {
+    "revision": "df5d928829edf6f3bec9",
+    "url": "/Back-to-the-Future/static/js/runtime-main.13387317.js"
+  }
+]);
---
+++ static/js/main.5e854a8b.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpback-to-the-future"]=this["webpackJsonpback-to-the-future"]||[]).push([[0],[,,,function(e,t,n){e.exports=n(4)},function(e,t,n){"use strict";n.r(t);var o=n(0),r=n.n(o),u=n(2);n.n(u).a.render(r.a.createElement("h1",null,"Hello world"),document.getElementById("root"))}],[[3,1,2]]]);
+//# sourceMappingURL=main.5e854a8b.chunk.js.map
