78c20c88fd2c3b48cf15c260395d05a9c61b2957
Atala <atala.rchidi@gmail.com>
2020-02-18 23:06:14 -0500

    Updates

--- precache-manifest.726ab070bba25221347d0fc6091b8c5f.js
+++ precache-manifest.726ab070bba25221347d0fc6091b8c5f.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "134c7e57721687b7296b4784a4f82174",
+    "revision": "0e6900aade83a606a41a6889670ae181",
     "url": "/Back-to-the-Future/index.html"
   },
   {
-    "revision": "e9f475e709583a12a18f",
-    "url": "/Back-to-the-Future/static/css/main.dbb11f22.chunk.css"
+    "revision": "85987cc210571d8c5341",
+    "url": "/Back-to-the-Future/static/css/main.aaa4f679.chunk.css"
   },
   {
-    "revision": "89aaf9f8741efd63e443",
-    "url": "/Back-to-the-Future/static/js/2.2e50a402.chunk.js"
+    "revision": "964f83d5ef2672a96c8a",
+    "url": "/Back-to-the-Future/static/js/2.e7160ec3.chunk.js"
   },
   {
     "revision": "9cd56dec2bfb059c19973edf344033c7",
-    "url": "/Back-to-the-Future/static/js/2.2e50a402.chunk.js.LICENSE.txt"
+    "url": "/Back-to-the-Future/static/js/2.e7160ec3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e9f475e709583a12a18f",
-    "url": "/Back-to-the-Future/static/js/main.89aea9fb.chunk.js"
+    "revision": "85987cc210571d8c5341",
+    "url": "/Back-to-the-Future/static/js/main.28cf0440.chunk.js"
   },
   {
     "revision": "df5d928829edf6f3bec9",
@@ -34,6 +34,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "0730d498edd9f20ecca892f8804b28b6",
     "url": "/Back-to-the-Future/static/media/desert.0730d498.jpg"
+  },
+  {
+    "revision": "64e5c8a417f04e97fd1be051f9881c0a",
+    "url": "/Back-to-the-Future/static/media/ltom.64e5c8a4.jpg"
   },
   {
     "revision": "21b05b9ab14c3abbb5bc073f9c08b7db",
