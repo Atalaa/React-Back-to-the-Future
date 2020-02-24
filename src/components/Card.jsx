a597c44d881914c38054ae5a75e068350842284f
Atala <atala.rchidi@gmail.com>
2020-02-23 23:24:27 -0500

    added component card and styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,7 +7,15 @@ function Card(){
     return(
         <div className="card">
             <div className="card__side card__side--front">
-                FRONT
+                <div className="card__picture--1">
+                    &nbsp;
+                </div>
+                <div className="card__heading">
+                    Heading
+                </div>
+                <div className="card__details">
+                    Details about movie
+                </div>
             </div>
             <div className="card__side card__side--back card__side--back-1">
                 BACK
