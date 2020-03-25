534b5649450c18540841699ede2cd924d95a2b6f
Atala <atala.rchidi@gmail.com>
2020-03-25 11:11:04 -0400

    added new images

--- src/json/json_review_reviews.js
+++ src/json/json_review_reviews.js
@@ -1,29 +1,24 @@
 import review1 from '../img/review1.png';
 import review2 from '../img/review2.png';
 import review3 from '../img/review3.png';
-import review4 from '../img/review4.png';
+import test from '../img/test.jpg';
 
 
 const json_review_reviews = [
     {id: 1, 
     pic: review1,
     header:"My All Time Favorite Movie!",
-    desc:"My favorite thing about this movie is the relationship between Doc and Marty. Zemeckis and Gale have given the movie a core of feeling that makes real claims on us. For all its comedy, Back to the Future is about a kid coming to terms with his parents' inadequacies, a moment familiar to everyone, and the fulcrum in growing up."},
+    desc:"My favorite thing about this movie is the relationship between Doc and Marty. Zemeckis and Gale have given the movie a core of feeling that makes real claims on us."},
 
     {id: 2, 
     pic: review2,
     header:"the best 80's film ever!",
-    desc:"This film is just one of a kind. The charm and wit are so good it will have you wonder how they came up with such a concept, Michael J Fox is at his best as are the rest of the cast. Humour,comedy, science fiction, this film has it all. This film will remain timeless,I would urge anyone who loves films to add this one to their list now."},
+    desc:"This film is just one of a kind. The charm and wit are so good it will have you wonder how they came up with such a concept. This film will remain timeless, I would urge anyone to add it to their list now."},
 
     {id: 3, 
     pic: review3,
     header:"One of the best movies I have ever seen.",
-    desc:"Back to the future is probably one of the best movies if not the best movie I have ever seen. This movie is just fantastic, awesome and flawless. The action and suspense of this movie still excites to this day. You can watch this movie over and over again and still be thrilled. Most likely This is easily one of the best movies ever made."},
-
-    {id: 4, 
-    pic: review4,
-    header:"Best ever... Period.",
-    desc:"What else needs to be said? Anyone that knows anything about filmmaking knows that this is the best film ever made. Try watching certain movies over and over and see how quickly you get sick of it. This movie is different. The depth and richness of storytelling, the characters, oh my, the characters. "}
+    desc:"Back to the future is probably one of the best movies I have ever seen. It is just fantastic, awesome and flawless. You can watch this movie over and over again and still be thrilled. One of the best movies ever made."}
 ];
 
 export default json_review_reviews;
