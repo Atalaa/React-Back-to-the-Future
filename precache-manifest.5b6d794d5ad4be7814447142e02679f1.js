404e2b8f23f822b8a28196cab8f81188cb56f488
Atala <atala.rchidi@gmail.com>
2020-04-06 10:22:19 -0400

    Updates

--- precache-manifest.5b6d794d5ad4be7814447142e02679f1.js
+++ precache-manifest.5b6d794d5ad4be7814447142e02679f1.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "767cdd137ba3e5604d63a74d12eb90f0",
+    "revision": "22861fa31614e37322becba5c4c59023",
     "url": "/Back-to-the-Future/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Back-to-the-Future/static/css/2.d5660b37.chunk.css"
   },
   {
-    "revision": "9479766c9cb9825dd601",
-    "url": "/Back-to-the-Future/static/css/main.3b288e11.chunk.css"
+    "revision": "e773ca6a7386b6032522",
+    "url": "/Back-to-the-Future/static/css/main.827eb583.chunk.css"
   },
   {
     "revision": "abef35497422aa224ff6",
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Back-to-the-Future/static/js/2.6a9f2381.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9479766c9cb9825dd601",
+    "revision": "e773ca6a7386b6032522",
     "url": "/Back-to-the-Future/static/js/main.1a5b233a.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Back-to-the-Future/precache-manifest.76938f5a92b56edcc172a74233278c94.js"
+  "/Back-to-the-Future/precache-manifest.5b6d794d5ad4be7814447142e02679f1.js"
 );
 
 self.addEventListener('message', (event) => {
