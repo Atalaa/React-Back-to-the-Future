b487c7aba3dbfb0588908de0698683e0195cb788
Atala <atala.rchidi@gmail.com>
2020-04-20 11:31:50 -0400

    customed Audio component functionality

--- src/components/main_components/music/Audio.jsx
+++ src/components/main_components/music/Audio.jsx
@@ -7,12 +7,11 @@ function Audio({ idArtist, songArtist }){
         const musicBoxContainer = document.getElementsByClassName("musicBox")[0];
 
         musicBoxContainer.classList.add('playing');
+        musicBoxContainer.classList.remove('stop');
 
         document.getElementsByClassName("playSong")[0].querySelector('i.fa').classList.remove('fa-play');
         document.getElementsByClassName("playSong")[0].querySelector('i.fa').classList.add('fa-pause');
         myAudio.play();
-
-
     }
 
     function pauseAudio(){
@@ -23,8 +22,21 @@ function Audio({ idArtist, songArtist }){
 
         document.getElementsByClassName("playSong")[0].querySelector('i.fa').classList.remove('fa-pause');
         document.getElementsByClassName("playSong")[0].querySelector('i.fa').classList.add('fa-play');
-
         myAudio.pause();
+    }
+
+    function stopAudio(){
+        const myAudio = document.getElementsByClassName("audio-element")[0];
+        const musicBoxContainer = document.getElementsByClassName("musicBox")[0];
+
+        musicBoxContainer.classList.remove('playing');
+        musicBoxContainer.classList.add('stop');
+
+
+        document.getElementsByClassName("playSong")[0].querySelector('i.fa').classList.remove('fa-pause');
+        document.getElementsByClassName("playSong")[0].querySelector('i.fa').classList.add('fa-play');
+        myAudio.pause();
+        myAudio.currentTime = 0;
     }
 
     function handleClick(){
@@ -37,14 +49,32 @@ function Audio({ idArtist, songArtist }){
         }
     }
 
+    function handleClickStop(){
+        stopAudio();
+    }
+
+
+
     return(
-        <div>
-            <button className="playSong">
-                <i className="fa fa-play fa-3x" onClick={handleClick}></i>
-            </button>
+        <div className="audio-set">
+
+            <div className="audio-playSong">
+                <button className="playSong">
+                    <i className="fa fa-play fa-3x" onClick={handleClick}></i>
+                </button>
+            </div>
+
+            <div className="audio-stopSong">
+                <button className="stopSong">
+                    <i className="fa fa-stop fa-3x" onClick={handleClickStop}></i>
+                </button>
+            </div>
 
             <audio className="audio-element" src={songArtist}></audio>
+
         </div>
+        
+
     )        
 }
 
