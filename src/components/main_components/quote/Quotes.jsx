6ad581ca84e01bc8dca609d33fba3f9cb688645b
Atala <atala.rchidi@gmail.com>
2020-05-09 10:51:33 -0400

    added js code

--- src/components/main_components/quote/Quotes.jsx
+++ src/components/main_components/quote/Quotes.jsx
@@ -19,7 +19,36 @@ function Quote(){
     });
     
 
-    
+    function quote(){        
+        fetch("https://andruxnet-random-famous-quotes.p.rapidapi.com/?cat=movies&count=10", {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "andruxnet-random-famous-quotes.p.rapidapi.com",
+                "x-rapidapi-key": "hjhBt7XE1Kmsh2YInKDyvKxfSPY4p1kY06cjsnz9ck6MUQqKdV"
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            textRef.current.innerHTML = `"${data[0].quote}"`;
+            movieRef.current.innerHTML = `- ${data[0].author} -`;
+            quoteLeftRef.current.classList.add("fa", "fa-quote-left", "fa-3x");
+            quoteRightRef.current.classList.add("fa", "fa-quote-right", "fa-3x");
+
+            if(bgColorBool){
+                bgColorRef.current.style.backgroundImage = "linear-gradient(45deg, #000, #fff)";
+                bgColorH2Ref.current.style.backgroundImage = "linear-gradient(to bottom, #240b36, #11998e)";
+                testRef.current.style.backgroundImage = "linear-gradient(45deg, #000, #fff)";
+                bgColorBool = false;
+            }else{
+                bgColorRef.current.style.backgroundImage = "linear-gradient(45deg, #480048, #c04848)";
+                bgColorH2Ref.current.style.backgroundImage = "linear-gradient(to bottom, #ee0979, #ff6a00)";
+                testRef.current.style.backgroundImage = "linear-gradient(45deg, #480048, #c04848)";
+                bgColorBool = true;
+            }
+        })
+        .catch(err => console.log('error is ', err))    
+    }
+
 
     return(
         <section ref={bgColorRef} className="sectionQuotes utility-center-text">
