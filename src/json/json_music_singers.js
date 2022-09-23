4a73ebcdd2d1d32116dbf1d5dffcd7563092aa88
Atala <atala.rchidi@gmail.com>
2022-09-23 14:40:24 -0400

    updated the correct mp3 song, built and deploy

--- src/json/json_music_singers.js
+++ src/json/json_music_singers.js
@@ -1,10 +1,14 @@
-import huey from '../img/hlewis.jpg';
-import soundHuey from '../sounds/back_in_time.mp3';
+import huey from "../img/hlewis.jpg";
+import soundHuey from "../sounds/Huey Lewis & The News - The Power Of Love (HQ).mp3";
 
-
-const json_music_singers = [ 
-    {id: 1, image: huey,  mp3: soundHuey, artist:"Huey Lewis and the News",
-    info: "Their greatest success was \"The Power of Love\" as the soundtrack of Back to the Future."}
+const json_music_singers = [
+  {
+    id: 1,
+    image: huey,
+    mp3: soundHuey,
+    artist: "Huey Lewis and the News",
+    info: 'Their greatest success was "The Power of Love" as the soundtrack of Back to the Future.',
+  },
 ];
 
-export default json_music_singers; 
+export default json_music_singers;
